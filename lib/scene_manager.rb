ef9ecb67ca684244cd50231b5db0d90803aa557b
Brad Swords <brad.swords@me.com>
2015-04-06 15:15:42 -0500

    updated scene descriptions

--- lib/scene_manager.rb
+++ lib/scene_manager.rb
@@ -27,19 +27,20 @@ class SceneManager < SavableObject
 
 		@items[:coconut] ||= Item.new({ delegate: @delegate })
 		@items[:coconut].load_unsaved_data({ name: "coconut",
-			description: "Hm, this coconut must have fallen of a tree.", article: "a"
+			description: "Hm, this coconut must have fallen off a tree.", article: "a"
 		})
 
 		# SCENES:
 
 		@scenes[:shore] ||= Scene.new({ delegate: @delegate})
 		@scenes[:shore].load_unsaved_data({ name: "Island Shore",
-			description: "You are on the shore of a small, gloomy island.\nThe shore continues east. There is a mysterious jungle north."
+			# add some sort of "you wake up" before this
+			description: "You are on the shore of a small, gloomy island.\nThe ocean is eerily calm, causing a thick silence to\ncoat the shore. The beach continues east.\nThere is a large jungle to the north."
 		})
 
 		@scenes[:shore_east] ||= Scene.new({ delegate: @delegate })
 		@scenes[:shore_east].load_unsaved_data({ name: "Island Shore",
-			description: "The shore continues west."
+			description: "The shore continues to stretch to the west."
 		})
 
 		@scenes[:jungle] ||= Scene.new({ delegate: @delegate,
@@ -49,7 +50,7 @@ class SceneManager < SavableObject
 		})
 		@scenes[:jungle].load_unsaved_data({ name: "Jungle",
 			# make this poetic
-			description: "The trees are very tall here, theres not much\nlight coming through. The beach is south."
+			description: "The trees are very tall here, and there's not much\nlight coming through. The beach is to the south."
 		})
 
 		@scenes[:shore].paths = { n: @scenes[:jungle], e: @scenes[:shore_east] }
